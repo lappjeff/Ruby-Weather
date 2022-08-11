de66b9258d377eaed5706a36dbf56c74ae1988db
Jeff Lapp <lappjeff20@gmail.com>
2022-08-11 15:01:45 -0400

    display bar chart in browser

--- main.rb
+++ main.rb
@@ -13,3 +13,24 @@ geodata = GeoData.new(public_ip)
 weather = Weather.new(geodata.printLatitude, geodata.printLongitude)
 
 weather.reportWeather
+
+
+print "\n View chart data[y,n]?"
+
+view_chart_data = gets.chomp
+
+if(view_chart_data != 'y' and view_chart_data != 'yes') 
+    return 
+end 
+
+barChart = BarChart.new('bvg', "Min|Max")
+
+minSerie = weather.printMinTemps.join(',')
+maxSerie = weather.printMaxTemps.join(',')
+
+series = [minSerie, maxSerie]
+
+chartLink = barChart.generateLink(series, weather.printDates)
+
+
+Launchy.open(chartLink)
