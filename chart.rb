60aff2cf13063e28aa2be868fe851919d68c395f
Jeff Lapp <lappjeff20@gmail.com>
2022-08-11 14:28:13 -0400

    create barchart child class

--- chart.rb
+++ chart.rb
@@ -4,3 +4,12 @@ class Chart
     @@chart_uri = "https://image-charts.com/chart?chs=999x400"
     @@uri_delimiter = "|"
 end 
+
+class BarChart < Chart 
+
+    def initialize(barType, legend)
+        @@chart_uri = @@chart_uri + "&cht=#{barType}&chma=0,0,20,10&chxt=x,y&chdl=#{legend}"
+    end
+
+end
+
