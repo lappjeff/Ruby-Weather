f950e8f58ae0053594f8d71b5ed9d95ac21e2d3e
Jeff Lapp <lappjeff20@gmail.com>
2022-08-11 14:58:05 -0400

    create method to fetch coordinate data

--- geodata.rb
+++ geodata.rb
@@ -9,7 +9,24 @@ class GeoData
 
     def initialize(ip)
         @ip = ip
-    end 
+    def fetchCoordinates
+        geocode_api_key = ENV["GEOCODE_API_KEY"]
+
+        if not geocode_api_key
+            raise 'No Geocode api key provided. Please follow instructions in readme for Geocode api key provision.'
+        end 
+
+        geodata_res = Net::HTTP.get(URI("https://geocode.xyz/#{@ip}?json=1&auth=#{geocode_api_key}"))
+
+        geodata = JSON.parse(geodata_res)        
+        
+        @longitude = geodata['longt']
+        @latitude = geodata['latt']
+
+    end
+
+    private :fetchCoordinates
+
     def printLatitude 
         @latitude
     end  
