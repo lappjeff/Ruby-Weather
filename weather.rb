be2017f1af002a8417fc2f2e234c6505765c5f0d
Jeff Lapp <lappjeff20@gmail.com>
2022-08-11 15:00:22 -0400

    create method to print weather data to console

--- weather.rb
+++ weather.rb
@@ -20,4 +20,27 @@ class Weather
     def printMaxTemps
         @max_temps
     end 
+
+    def reportWeather()
+
+        i = 0
+
+        string_break = "-----------------------|"
+
+        puts string_break
+
+        while i < @dates.length
+            date = @dates[i]
+            top_temp = @max_temps[i]
+            min_temp = @min_temps[i]
+
+            puts " Date: #{date}      |"
+            puts " Top temperature: #{top_temp} |"
+            puts " Min temperature: #{min_temp} |"
+            puts "#{string_break}\n"
+
+            i += 1
+        end 
+    end
+
 end
